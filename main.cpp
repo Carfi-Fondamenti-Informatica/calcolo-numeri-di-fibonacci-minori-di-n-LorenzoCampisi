b339922d93ed36f15ea8b7f43ae6c184ceca80c2
LorenzoCampisi <91668709+LorenzoCampisi@users.noreply.github.com>
2021-11-10 11:28:06 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,33 @@
 using namespace std;
 
 int main() {
-   return 0;
+
+    int n=0, primo=1 , secondo=1 , terzo=0;
+
+    cout<<"inserisci numero"<<endl;
+    cin>>n;
+
+    if(n>=2){
+        cout<<primo<<endl;
+        cout<<secondo<<endl;
+
+        for(int i=2;terzo<(n-2);i++){
+            terzo=primo+secondo;
+            primo=secondo;
+            secondo=terzo;
+            cout<<terzo<<endl;
+        }
+
+    }
+    else{
+        cout<<"errore"<<endl;
+    }
+
+
+
+
+    return 0;
+
+
+
 }
