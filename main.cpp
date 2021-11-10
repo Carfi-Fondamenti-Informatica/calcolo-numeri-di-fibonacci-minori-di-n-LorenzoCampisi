cdc8a9ccc8ef2524a6efcb7e8d6e39cd742483ac
LorenzoCampisi <91668709+LorenzoCampisi@users.noreply.github.com>
2021-11-10 18:54:50 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,30 +1,20 @@
 #include <iostream>
 using namespace std;
+int main(){
+int n;
+cout << "inserisci n:";
+cin >> n;
 
-int main() {
-
-    int n=0, primo=1 , secondo=1 , terzo=0;
-
-    cout<<"inserisci numero"<<endl;
-    cin>>n;
-
-    if(n>=2){
-        cout<<primo<<endl;
-        cout<<secondo<<endl;
-
-        for(int i=2;i<=n;i++) {
-            terzo = primo + secondo;
-            primo = secondo;
-            secondo = terzo;
-            if (terzo > n) break;
-            else cout << terzo << endl;
-        }
-
-    }
-    else{
-        cout<<"errore"<<endl;
-    }
-
-    return 0;
-
+int long primo = 0, secondo = 1, numero = primo + secondo;
+int i = 0;
+while (numero <= n) {
+if (i == 0) cout << 1 << endl;
+else {
+cout << numero << endl;
+primo = secondo;
+secondo = numero;
+numero = secondo + primo;
+}
+i++;
+}
 }
